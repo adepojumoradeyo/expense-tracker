a2c1f75a8279d34248f89de4f3a50dabe2b24d60
Phoebe <https://github.com/adepojumoradeyo/movie-search-app>
2026-04-29 10:54:01 -0700

    ui design

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -6,7 +6,7 @@ export default function TransactionList({ transactions, onDelete }) {
       <div className="px-4 pt-6 mt-10">
         <h3 className="text-3xl font-semibold  mb-3">Transactions </h3>
         <div className=" py-5 ">
-          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-h-[70vh] overflow-y-auto">
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {(transactions || []).map((tx) => (
               <TransactionItem
                 key={tx.id}
